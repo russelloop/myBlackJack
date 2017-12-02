5d87e609eecd313da87272bec4e50a2e0c4d5bb6
russelloop <reasonsample@outlook.com>
2017-12-02 19:11:35 +0800

    new file Card

---
+++ src/Card.java
@@ -0,0 +1,18 @@
+public class Card {
+    /**
+     * @param value: 牌的面值，1~10，花牌（J、K、Q）计作10
+     * @param type: 花色，分别用“1,2,3,4”代表“黑桃，红桃，梅花，方片”
+     */
+    private int value;//面值
+    private int type;//花色
+    public Card(int v, int t){
+        this.value = v;
+        this.type = t;
+    }
+    public int getValue(){
+        return value;
+    }
+    public int getType(){
+        return type;
+    }
+}
